d093b413e1b34cfea911ae30dd6370282428f23f
Bart Massey <bart.massey@gmail.com>
2024-07-31 01:14:22 -0700

    added forgotten actual example code; holy something

---
+++ examples/mb2-wukong.rs
@@ -0,0 +1,36 @@
+#![no_main]
+#![no_std]
+
+use ws2812_hal_delay::Ws2812;
+use smart_leds::RGB8;
+use smart_leds_trait::SmartLedsWrite;
+
+use cortex_m_rt::entry;
+use embedded_hal::delay::DelayNs;
+use microbit::{board::Board, hal::{gpio::{Level, DriveConfig}, Delay, Timer}};
+use panic_rtt_target as _;
+use rtt_target::rtt_init_print;
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    let board = Board::take().unwrap();
+    let delay = Delay::new(board.SYST);
+    let mut timer = Timer::new(board.TIMER0);
+    let pin = board.edge.e16.into_push_pull_output_drive(
+        Level::Low,
+        DriveConfig::HighDrive0HighDrive1,
+    );
+    let mut ws2812 = Ws2812::new(delay, pin);
+    
+    let leds = [
+        RGB8::new(255u8, 0, 0),
+        RGB8::new(0, 255u8, 0),
+        RGB8::new(0, 0, 255u8),
+        RGB8::new(255u8, 255u8, 255u8),
+    ];
+    loop {
+        <Ws2812<_, _> as SmartLedsWrite>::write(&mut ws2812, leds.into_iter()).unwrap();
+        timer.delay_ms(500);
+    }
+}
